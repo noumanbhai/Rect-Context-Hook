e5030c04d5cb1d3e6179061bffbe960709bfd884
Nouman <noumanbhai006.com>
2021-09-28 17:44:29 +0500

    botstrap add and navbar and router link add

--- src/App.js
+++ src/App.js
@@ -1,19 +1,30 @@
 import './App.css';
-import Aboute from './components/Aboute';
-import Footer from "./components/Footer";
-import Header from "./components/Header";
-import NoteState from './context/notes/NoteState';
-
+import Navbar from './components/Navbar';
+import React from "react-router-dom";
+import Home from './components/Home';
+import About from './components/About';
+import Contact from './components/Contact';
+// import React from "react";
+// import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 function App() {
   return (
-    <div className="App">
-      <NoteState >
-      <h1>Book</h1>
-      <Aboute />
-      <Footer/>
-      <Header></Header>
-      </NoteState >
-    </div>
+    <>
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route exact path="/about">
+            <About />
+          </Route>
+          <Route exact path="/contact">
+            <Contact />
+          </Route>
+        </Switch>
+      </Router>
+    </>
   );
 
 }
---
+++ src/components/Contact.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Contact() {
+    return (
+        <div className="App">
+            <h1>This is contct page</h1>
+        </div>
+    )
+}
+
+export default Contact
